1eb0c566eb7f02ef5bfc1c73db7552eaa474d476
Josiah De La Torre <jodelat@yahoo.com>
2018-01-23 13:33:53 -0800

    Profile Component

---
+++ client/src/components/Profile.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Jumbotron} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+
+const Profile = props => (
+
+  <Jumbotron>
+    <div className="row">
+      <div className="col-sm-3">
+        <img src={props.picture} />
+      </div>
+      <div className="col-sm-9">
+        <div className="row">
+          <h3>{props.username}</h3>
+          <Button>Edit Profile</Button>
+        </div>
+        <div className="row">
+          <h5>{props.posts}</h5>
+          <p>posts</p>
+          <h5>{props.followers}</h5>
+          <p>followers</p>
+          <h5>{props.following}</h5>
+          <p>following></p>
+        </div>
+        <div className="row">
+          <h4>{props.name}</h4>
+        </div>
+      </div>
+    </div>
+  </Jumbotron>
+
+);
