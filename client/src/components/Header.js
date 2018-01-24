5fd07ef0fccc1330e097c44d95cd2ca1f8595e15
brhelm <brhelm77@gmail.com>
2018-01-23 16:27:08 -0800

    facebook OAuth structure

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,15 +9,17 @@ class Header extends Component {
             case null:
                 return;
             case false:
-                return (<li><a href="/auth/google">Login With Google</a></li>)
+                return [<li key="1"><a href="/auth/facebook">Login With FaceBook</a></li>,
+                        <li key="2"><a href="/auth/google">Login With Google</a></li>
+                ];
             default:
-        return [
-            <li key="1"><Payments /></li>,
-            <li key="3" style={{ margin: '0 10px'}}>
-                Credits: {this.props.auth.credits}
-            </li>,
-            <li key="2"><a href="/api/logout">Logout</a></li>
-        ];
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="3" style={{ margin: '0 10px'}}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
