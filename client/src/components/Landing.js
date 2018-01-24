a0d1b135211f15784c6057ae7d8d952da47bf3b4
brhelm <brhelm77@gmail.com>
2018-01-24 15:13:25 -0800

    heroku test 3

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Landing = () => {
     return (
+        <div>MyStorya</div>,
         <div style={{ textAlign: 'center'}}>
-        <h1>
-            Emaily!
-        </h1>
-        Collect feedback from your users
+            <img src={require('./img/pileOfBooks1.png')} alt="MyStorya.com" />
         </div>
     );
 };
