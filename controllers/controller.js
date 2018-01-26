58310fd9beb2b0bd6a1c92606dceccf9b666f6c4
Josiah De La Torre <jodelat@yahoo.com>
2018-01-26 00:08:17 -0800

    fixed syntax error and added path module to package.json

--- controllers/controller.js
+++ controllers/controller.js
@@ -32,7 +32,7 @@ module.exports = {
       .sort({views:1})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  },
   create: function(req, res) {
     db.Book
       .create(req.body)
